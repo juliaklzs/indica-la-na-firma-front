7449045c5073f9be55bacdb6e5f0f00ec4216f98
LucasDevSystem <highspeedsetup@gmail.com>
2023-02-24 22:02:13 -0300

    Feat (NavBar): navigation top bar

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,36 @@
+import { AppBar, Avatar, IconButton, Tooltip } from "@mui/material";
+import { Box, Container } from "@mui/system";
+
+import Logo from "../../assets/Logo.jpg"
+
+const NavBar = () => {
+  return (
+    <AppBar style={{ backgroundColor: "#50bde4" }} position="static">
+      <Container maxWidth="xl">
+        <Box sx={{ height: 50, flexGrow: 0 }}>
+          <Box
+            component="img"
+            sx={{
+              height: 50,
+              width: 350,
+              maxHeight: { xs: 233, md: 167 },
+              maxWidth: { xs: 350, md: 250 },
+            }}
+            alt="The house from the offer."
+            src={Logo}
+          />
+          <Tooltip style={{ float: "right" }} title="Open settings">
+            <IconButton sx={{ p: 0 }}>
+              <Avatar
+                alt="Remy Sharp"
+                src="https://avatars.githubusercontent.com/u/36340073?s=400&u=2426b833712b87aac6bd12ed684481c4219a9cd2&v=4"
+              />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default NavBar;
